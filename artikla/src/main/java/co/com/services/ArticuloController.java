69b848c7ae3a42ac668f16d5e649c4e12f0d9355
Daniel Quintero Forero <daniel.quintero@swlisim.com>
2020-05-24 17:51:50 -0500

    ArticuloController Ajustes

--- artikla/src/main/java/co/com/services/ArticuloController.java
+++ artikla/src/main/java/co/com/services/ArticuloController.java
@@ -46,47 +46,45 @@ public class ArticuloController  {
 		return articuloRepository.findById(1);
 	}
         
-<<<<<<< HEAD
 	@RequestMapping(value="/addArticulo") 
     public boolean buscarArticulo(@RequestBody int articulo ){
         System.out.println(articulo);
         return true;
     }
-=======
-        @RequestMapping("/traerArticulos")
-        public List<Articulo> buscarArticulos(){
-            return articuloRepository.findAll();
-        }
+
+    @RequestMapping("/traerArticulos")
+    public List<Articulo> buscarArticulos(){
+        return articuloRepository.findAll();
+    }
         
-        @RequestMapping(value="/agregarArticulo", method= RequestMethod.POST) 
-        public Articulo buscarArticulo(@RequestBody ArticuloDto articuloDto ){
+    @RequestMapping(value="/agregarArticulo", method= RequestMethod.POST) 
+    public Articulo buscarArticulo(@RequestBody ArticuloDto articuloDto ){
             
-            Rol rol = new Rol();
-            rol.setDescripcion("autor");
-            
-            Autor autor = new Autor();
-            autor.setId(1);
-            autor.setRol("autor");
-            autor.setNombre("autor1");
-            
-            Articulo articulo = new Articulo();
-            articulo.setTitulo(articuloDto.getDescripcion());
-            articulo.setDescripcion(articuloDto.getDescripcion());
-            articulo.setAutor(autor);
-            
-            Categoria categoria = new Categoria();
-            categoria.setId(2);
-            categoria.setTitulo("Metodológicos");
-            categoria.setDescripcion("Con los artículos metodológicos se busca dar nuevas metodologías o modificar las que ya se dieron luego de un minucioso análisis de las mismas.");
-            
-            List<Categoria> categorias = new ArrayList<>();
-            
-            categorias.add(categoria);
-            
-            articulo.setCategorias(categorias);
-            
-            return articuloRepository.save(articulo);
-        }
->>>>>>> add103dbcacc40db1d843415f73b0f4599733cc8
+        Rol rol = new Rol();
+        rol.setDescripcion("autor");
+        
+        Autor autor = new Autor();
+        autor.setId(1);
+        autor.setRol("autor");
+        autor.setNombre("autor1");
+        
+        Articulo articulo = new Articulo();
+        articulo.setTitulo(articuloDto.getDescripcion());
+        articulo.setDescripcion(articuloDto.getDescripcion());
+        articulo.setAutor(autor);
+        
+        Categoria categoria = new Categoria();
+        categoria.setId(2);
+        categoria.setTitulo("Metodológicos");
+        categoria.setDescripcion("Con los artículos metodológicos se busca dar nuevas metodologías o modificar las que ya se dieron luego de un minucioso análisis de las mismas.");
+        
+        List<Categoria> categorias = new ArrayList<>();
+        
+        categorias.add(categoria);
+        
+        articulo.setCategorias(categorias);
+        
+        return articuloRepository.save(articulo);
+    }
 
 }
